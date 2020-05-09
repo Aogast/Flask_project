fb00826ae09bda03e0e0de075388079817d62056
NoktisLucisKelum <55601448+NoktisLucisKelum@users.noreply.github.com>
2020-05-09 14:19:25 +0300

    Merge branch 'master' of https://github.com/HTSK-company/Flask_project into interface
     Conflicts:
    	templates/main.html
    	templates/Инструкция.html

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from flask import Flask, url_for, request, render_template, redirect
-
+from pickle import loads, dumps
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
+
+@app.route('/instr')
+def instr():
+    return render_template('Инструкция.html')
+
+
+@app.route('/table')
+def tab():
+    return render_template('ordertable.html', user='x')
+
+
+@app.route('/ord', methods=['POST', 'GET'])
+def order():
+    if request.method == 'POST':
+        n = {'name': request.form['name'],
+                'surname': request.form['surname'],
+                'adress': request.form['adress'],
+                'telefon': request.form['telnum'],
+                'city': request.form['city'],
+                'amount': request.form['cooknumber'],
+                'surgeter': request.form['surgeter'],
+                'cv': request.form['CVV'],
+                'kindofcook': request.form['kindof'],
+                'time': request.form['time']}
+        b = dumps(n)
+        print(n)
+        return redirect(url_for('tab'))
+    else: return render_template('makeorder.html')
 
 
 if __name__ == '__main__':
