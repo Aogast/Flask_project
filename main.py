31205c42abc79998a8ed7864977af167f6fd71af
syrov <syrov.art@gmail.com>
2020-05-10 15:30:21 +0300

    all_1 release

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ db_session.global_init("db/blogs.sqlite")
 
 @app.route('/')
 def index():
-    '''main page'''
+    """main page"""
     return render_template('main.html')
 
 
 @app.route('/instr')
 def instr():
-    '''instruction for using'''
+    """instruction for using"""
     return render_template('Инструкция.html')
 
 
 @app.route('/table')
 def tab():
-    '''page with a table of orders'''
+    """page with a table of orders"""
     return render_template('ordertable.html', user=js_info())
 
 
 @app.route('/ord', methods=['POST', 'GET'])
 def order():
-    '''function, which makes orders and creates json'''
+    """function, which makes orders and creates json"""
     if request.method == 'POST':
         filejs = {'name': request.form['name'],
              'address': request.form['address'],
@@ -51,7 +51,7 @@ def order():
 
 
 def js_info():
-    '''function, which creates database'''
+    """function, which creates database"""
     session = db_session.create_session()
     if os.access('info.json', os.R_OK):
         with open('info.json') as f:
